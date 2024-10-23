f66a36af54fdde7b7ac7ada0dbcb9b19605932d4
bap2pecs <bap2pecs@proton.me>
2024-10-22 21:48:24 -0400

    clean up

--- scripts/babylon-integration/init-consumer-eots-dir.sh
+++ scripts/babylon-integration/init-consumer-eots-dir.sh
@@ -7,16 +7,19 @@ source $(pwd)/.env.babylon-integration
 set +a
 
 CONSUMER_EOTS_MANAGER_DIR=$(pwd)/.consumer-eotsmanager
-CONFIGS_DIR=$(pwd)/configs/babylon-integration
+EXAMPLE_CONFIG=$(pwd)/configs/babylon-integration/consumer-eotsd.conf
 EOTS_MANAGER_CONF=$(pwd)/.consumer-eotsmanager/eotsd.conf
 
 # Only run if the directory does not exist
 if [ ! -d "$CONSUMER_EOTS_MANAGER_DIR" ]; then
   echo "Creating $CONSUMER_EOTS_MANAGER_DIR directory..."
   mkdir -p $CONSUMER_EOTS_MANAGER_DIR
-  echo "Copying $CONFIGS_DIR/consumer-eotsd.conf to $EOTS_MANAGER_CONF..."
-  cp $CONFIGS_DIR/consumer-eotsd.conf $EOTS_MANAGER_CONF
+  echo "Copying $EXAMPLE_CONFIG to $EOTS_MANAGER_CONF..."
+  cp $EXAMPLE_CONFIG $EOTS_MANAGER_CONF
 
+
+  # TODO: 777 grants full read, write, and execute permissions to everyone 
+  # (owner, group, and others). This is not a good idea for production use
   chmod -R 777 $CONSUMER_EOTS_MANAGER_DIR
   echo "Successfully initialized $CONSUMER_EOTS_MANAGER_DIR directory"
   echo
--- scripts/babylon-integration/start-consumer-eotsmanager.sh
+++ scripts/babylon-integration/start-consumer-eotsmanager.sh
@@ -6,20 +6,12 @@ set -a
 source $(pwd)/.env.babylon-integration
 set +a
 
-CONSUMER_EOTS_MANAGER_DIR=$(pwd)/.consumer-eotsmanager
-CONFIGS_DIR=$(pwd)/configs/babylon-integration
 EOTS_MANAGER_CONF=$(pwd)/.consumer-eotsmanager/eotsd.conf
 
-# Only run if the directory does not exist
-if [ ! -d "$CONSUMER_EOTS_MANAGER_DIR" ]; then
-  echo "Creating $CONSUMER_EOTS_MANAGER_DIR directory..."
-  mkdir -p $CONSUMER_EOTS_MANAGER_DIR
-  echo "Copying $CONFIGS_DIR/consumer-eotsd.conf to $EOTS_MANAGER_CONF..."
-  cp $CONFIGS_DIR/consumer-eotsd.conf $EOTS_MANAGER_CONF
-
-  chmod -R 777 $CONSUMER_EOTS_MANAGER_DIR
-  echo "Successfully initialized $CONSUMER_EOTS_MANAGER_DIR directory"
-  echo
+# Check if the eotsd.conf file exists
+if [ ! -f "$EOTS_MANAGER_CONF" ]; then
+  echo "Error: $EOTS_MANAGER_CONF does not exist"
+  exit 1
 fi
 
 echo "Starting consumer-eotsmanager..."
@@ -28,7 +20,8 @@ docker compose -f docker/docker-compose-babylon-integration.yml up -d consumer-e
 echo "Waiting for consumer-eotsmanager to start..."
 sleep 5
 echo "Checking the docker logs for consumer-eotsmanager..."
-# This is a hardcoded check to verify if eotsmanager has started successfully.
+# TODO: This is a hardcoded check to verify if eotsmanager has started successfully.
+# We should find a better way to check if the service has started successfully.
 REQUIRED_LOG_MESSAGE="EOTS Manager Daemon is fully active"
 if ! docker compose -f docker/docker-compose-babylon-integration.yml logs consumer-eotsmanager | grep -q "$REQUIRED_LOG_MESSAGE"; then
     echo "Error: consumer-eotsmanager failed to start"
