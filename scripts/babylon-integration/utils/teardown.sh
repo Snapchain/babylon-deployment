5f9e1b8fb5953aa80df5784f5bfd71f01f4c4c3c
parketh <yeungparkhay@gmail.com>
2024-11-12 13:19:05 +0000

    remove duplicate volume

--- scripts/babylon-integration/utils/teardown.sh
+++ scripts/babylon-integration/utils/teardown.sh
@@ -4,14 +4,14 @@ set -uo pipefail
 source "./common.sh"
 
 # Get the consumer FP address
-CONSUMER_FP_DIR=/home/finality-provider/.fpd
-KEYRING_FILENAME=$(ls $CONSUMER_FP_DIR/keyring-test | grep '\.address$' | sed 's/\.address$//')
+KEYRING_DIR=/home/.babylond
+CONSUMER_FP_KEYRING_DIR=$KEYRING_DIR/$CONSUMER_FINALITY_PROVIDER_KEY
+KEYRING_FILENAME=$(ls $CONSUMER_FP_KEYRING_DIR/keyring-test | grep '\.address$' | sed 's/\.address$//')
 echo "Keyring filename: $KEYRING_FILENAME"
 CONSUMER_FP_ADDRESS=$(babylond keys parse "$KEYRING_FILENAME" --output json | jq -r '.formats[0]')
 echo "Consumer FP address: $CONSUMER_FP_ADDRESS"
 
 # Get the prefunded key address
-KEYRING_DIR=/home/.babylond
 PREFUNDED_ADDRESS=$(babylond keys show "$BABYLON_PREFUNDED_KEY" \
     --keyring-dir "$KEYRING_DIR" \
     --keyring-backend test \
@@ -34,7 +34,7 @@ fi
 
 # Otherwise, send out funds to prefunded key
 # Reserve 0.001 bbn = 1000 ubbn for gas
-CONSUMER_FP_KEYRING_DIR=$KEYRING_DIR/$CONSUMER_FINALITY_PROVIDER_KEY
+
 AMOUNT_TO_SEND=$((CONSUMER_FP_BALANCE - TRANSFER_GAS_COST))
 echo "Sending $AMOUNT_TO_SEND ubbn to prefunded key..."
 echo "Consumer FP keyring dir: $CONSUMER_FP_KEYRING_DIR"
