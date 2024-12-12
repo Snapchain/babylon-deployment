29cb7d3e7e265723cd70369cb9025814b5220e4b
bap2pecs <bap2pecs@proton.me>
2024-12-12 08:10:59 +0000

    more

--- scripts/babylon-integration/stop-finality-gadget.sh
+++ scripts/babylon-integration/stop-finality-gadget.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 echo "Stopping finality-gadget..."
 docker compose -f docker/docker-compose-babylon-integration.yml down finality-gadget
 
-echo "Removing finality-gadget directory..."
-rm -rf $(pwd)/.finality-gadget
+echo "Moving finality-gadget directory..."
+sudo mv $(pwd)/.finality-gadget $(pwd)/.finality-gadget-deprecated-$(date +%Y%m%d%H%M%S)
 
 echo "Stopped finality-gadget"
 echo
