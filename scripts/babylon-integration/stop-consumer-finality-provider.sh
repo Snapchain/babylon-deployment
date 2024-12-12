7130025c3c19839f55f336084bba979c9ef16f35
bap2pecs <bap2pecs@proton.me>
2024-12-12 08:05:42 +0000

    init

--- scripts/babylon-integration/stop-consumer-finality-provider.sh
+++ scripts/babylon-integration/stop-consumer-finality-provider.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 echo "Stopping consumer-finality-provider..."
 docker compose -f docker/docker-compose-babylon-integration.yml down consumer-finality-provider
 
-echo "Removing consumer-finality-provider directory..."
-sudo rm -rf $(pwd)/.consumer-finality-provider
+echo "Moving consumer-finality-provider directory..."
+sudo mv $(pwd)/.consumer-finality-provider $(pwd)/.consumer-finality-provider-deprecated-$(date +%Y%m%d%H%M%S)
 
 echo "Stopped consumer-finality-provider"
 echo
