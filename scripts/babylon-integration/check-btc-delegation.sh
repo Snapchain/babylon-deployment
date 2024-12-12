d291c77d4a10ac05da0a6cb6b717432a87df332f
bap2pecs <bap2pecs@proton.me>
2024-12-12 09:07:28 +0000

    nit: docstring

--- scripts/babylon-integration/check-btc-delegation.sh
+++ scripts/babylon-integration/check-btc-delegation.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 echo "Checking BTC delegation status..."
-echo "DELEGATION_ACTIVE means the delegation is active"
+echo "DELEGATION_ACTIVE means the delegation is active (if there are multiple delegations, monitor the latest one)"
 docker exec btc-staker /bin/sh -c "/bin/stakercli daemon list-staking-transactions"
 echo
