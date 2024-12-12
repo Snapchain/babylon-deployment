3ab0ae816fd96746042929d26c01f95b4f4c14d5
bap2pecs <111917526+bap2pecs@users.noreply.github.com>
2024-12-12 03:15:16 -0500

    fix: avoid using `rm -rf` (#50)

--- scripts/babylon-integration/stop-btc-staker.sh
+++ scripts/babylon-integration/stop-btc-staker.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 echo "Stopping btc-staker..."
 docker compose -f docker/docker-compose-babylon-integration.yml down btc-staker
 
-echo "Removing btc-staker directory..."
-sudo rm -rf $(pwd)/.btc-staker
+echo "Moving btc-staker directory..."
+sudo mv $(pwd)/.btc-staker $(pwd)/.btc-staker-deprecated-$(date +%Y%m%d%H%M%S)
 
 echo "Stopped btc-staker"
 echo
--- scripts/babylon-integration/stop-consumer-eotsmanager.sh
+++ scripts/babylon-integration/stop-consumer-eotsmanager.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 echo "Stopping consumer-eotsmanager..."
 docker compose -f docker/docker-compose-babylon-integration.yml down consumer-eotsmanager
 
-echo "Removing consumer-eotsmanager directory..."
-sudo rm -rf $(pwd)/.consumer-eotsmanager
+echo "Moving consumer-eotsmanager directory..."
+sudo mv $(pwd)/.consumer-eotsmanager $(pwd)/.consumer-eotsmanager-deprecated-$(date +%Y%m%d%H%M%S)
 
 echo "Stopped consumer-eotsmanager"
 echo
